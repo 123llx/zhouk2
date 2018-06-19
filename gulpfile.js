58dfd27227c4d2b4ac1473104cae709b07c1cb03
llx <1457680521@qq.com>
2018-06-19 09:46:50 +0800

    启服务等完成

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+//引入
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minCss = require('gulp-clean-css');
+var server = require('gulp-webserver');
+var path = require('path');
+var fs = require('fs');
+var url = require('url');
+var data = require('./data/data.json')
+    //编译压缩sass 
+gulp.task('sass', function() {
+    gulp.src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(minCss())
+        .pipe(gulp.dest('./src/css'))
+})
+
+//监听sass
+gulp.task('watch', function() {
+    gulp.watch('./src/scss/index.scss', ['sass'])
+})
+
+//启服务
+gulp.task('server', ['sass'], function() {
+    gulp.src('./src')
+        .pipe(server({
+            port: 8080,
+            middleware: function(req, res) {
+                var pathname = url.parse(req.url).pathname;
+                if (pathname === '/favicon.ico') {
+                    return
+                }
+                if (pathname === '/api/list') {
+                    res.end(JSON.stringify(data))
+                } else {
+                    pathname = pathname === '/' ? '/index.html' : pathname
+                    res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)))
+                }
+
+            }
+        }))
+
+})
+
+//$
+gulp.task('default', ['sass', 'watch', 'server'])
